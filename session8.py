d31e59b707c0a3395a1e46c8890c22ccb929b4e5
BalajiB197 <balajib197@gmail.com>
2020-09-19 23:30:12 +0530

    added comments

--- session8.py
+++ session8.py
@@ -1,29 +1,73 @@
-def outer():
-    n = 50
+from functools import reduce
+
+def counter(fn):
+    '''
+    x = checks chars limit and it is free variable
+    returns True or False
+    '''
+    x = 50
     def inner():
-        """ 
-        closure is equal to inner function plus extended scope/free variable
-        """
-        result = inner.__doc__
-        if len(result) > n:
+        nonlocal x
+        if len(fn.__doc__) > x:
             return True
         else:
             return False
-    return inner()
+    return inner
 
+def fib(initial_value = 0):
+    '''
+    intial_value = 0, acts as cnt intializer
+    fib_series = creates a series of fib series considered first 100 fib nums
+    returns the next incremented index of fib series
+    '''
+    fib_series = lambda n: reduce(lambda x, _: x+[x[-1]+x[-2]], range(n-2), [0, 1])
+    series = fib_series(100)
+    def fib_loop(increment = 1):
+        nonlocal fib_series, initial_value
+        initial_value += increment
+        return series[initial_value]
+    return fib_loop
 
-def fibonacci(end):
-    n1 = 0
-    n2 = 1
-    count = 0
-    a = [0, 1]
-    def fib():
-        nonlocal n1, n2, count
-        while count < end:
-            sum1 = n1 + n2
-            n1 = n2
-            n2 = sum1
-            count += 1
-            a.append(sum1)
-    fib()
-    return a
+def closure_counts(fn):
+    '''
+    fn -> function name add, mul, div
+    cnt intialize the cnount to 0.
+    returns dictionary for all func name with counter.
+    '''
+    cnt = 0
+    global dict1 
+    dict1 = {}
+    def inner(*args, **kwargs):
+        nonlocal cnt
+        if fn.__name__ == 'add':
+            cnt += 1
+            dict1['add'] = cnt
+        elif fn.__name__ == 'mul':
+            cnt += 1
+            dict1['mul'] = cnt
+        elif fn.__name__ == 'div':
+            cnt += 1
+            dict1['div'] = cnt
+        return dict1
+    return inner
+
+def closure_counts_dict(fn, my_dict):
+    '''
+    fn -> function name add, mul, div
+    my_dict -> param dict
+    return -> dict for each method names div, mul, add
+    '''
+    cnt = 0
+    def inner(*args, **kwargs):
+        nonlocal cnt
+        if fn.__name__ == 'add':
+            cnt += 1
+            my_dict['add'] = cnt
+        elif fn.__name__ == 'mul':
+            cnt += 1
+            my_dict['mul'] = cnt
+        elif fn.__name__ == 'div':
+            cnt += 1
+            my_dict['div'] = cnt
+        return my_dict
+    return inner
--- test_session8.py
+++ test_session8.py
@@ -1,14 +1,95 @@
 import os
 import pytest
 import session8
-from session8 import *
+from session8 import counter
 
 
 def test_readme_exists():
     assert os.path.isfile("README.md"), "README.md file missing!"
 
-def test_doc_string():
-    assert session8.outer() == True
+def my_func():
+    # this is comment
+    """Passing this statement to check the
+    length of docstring is greater than 50 or not.
+    This function is passed as argument and the free variable
+    to check the length is assigned to var x
+    """
 
+def test_doc_string():
+    counter_add = session8.counter(my_func)
+    assert counter_add() == True
+
+def my_func2():
+    # this is comment
+    """Passing this statement to check the
+    """
+
+def test_doc_string2():
+    counter_add = session8.counter(my_func2)
+    assert counter_add() == False
+
+counter2 = session8.fib()
 def test_fib():
-    assert session8.fibonacci(6) == [0, 1, 1, 2, 3, 5, 8, 13]
+    assert counter2() == 1
+
+def test_fib2():
+    assert counter2() == 1
+
+def test_fib3():
+    assert counter2() == 2
+
+def test_fib4():
+    for _ in range(3):
+        counter2()
+    assert counter2() == 13
+
+def add(a, b):
+    return a + b
+def mul(a, b):
+    return a*b
+def div(a, b):
+    return a/b
+
+counter_add = session8.closure_counts(add)
+counter_mul = session8.closure_counts(mul)
+counter_div = session8.closure_counts(div)
+
+def test_closure_counts():
+    assert counter_mul(1,2) == {'mul': 1}
+
+def test_closure_counts2():
+    assert counter_add(1,2) == {'add': 1, 'mul': 1}
+
+def test_closure_counts3():
+    assert counter_mul(2,2) == {'add': 1, 'mul': 2}
+
+def test_closure_counts4():
+    assert counter_div(2, 2) == {'add': 1, 'div': 1, 'mul': 2}
+
+def test_closure_counts5():
+    for _ in range(3):
+        counter_div(5, 2)
+        counter_mul(5,2)
+        counter_add(5,2)
+    assert counter_div(3, 2) == {'add': 4, 'div': 5, 'mul': 5}
+
+dict1, dict2, dict3 = {}, {}, {}
+counter_dict_add = session8.closure_counts_dict(add, dict1)
+counter_dict_mul = session8.closure_counts_dict(mul, dict2)
+counter_dict_div = session8.closure_counts_dict(div, dict3)
+
+def test_closure_counts_dict1():
+    for _ in range(2):
+        counter_dict_add(2,3)
+    assert counter_dict_add(2, 2) == {'add': 3}
+
+def test_closure_counts_dict2():
+    for _ in range(4):
+        counter_dict_mul(2,3)
+    assert counter_dict_mul(2, 2) == {'mul': 5}
+
+def test_closure_counts_dict3():
+    for _ in range(10):
+        counter_dict_div(2,3)
+    assert counter_dict_div(2, 2) == {'div': 11}
+
